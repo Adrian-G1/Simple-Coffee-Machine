ee48c371f4f6766b5783036fea1c96e57c554346
Adrian Garcia <adrian@Adrians-MBP.lan>
2022-07-23 16:47:24 -0500

    updated main.py

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# vs_coffee
+# Simple Coffee Machine Program
+### Program will prompt users for given coffee selection:
+- Espresso
+- Cappuccino
+- Latte
+
+### Additionally, the following commands are also accepted:
+- Report
+#### Which would report current values of ingredients.
+
+- Off
+#### Breaks out of program.
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from pydoc import source_synopsis
+
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -31,41 +34,87 @@ resources = {
     "coffee": 100
 }
 
-money: float = 0.0
-
 
 # TODO: 1. Prompt user for input
 
-def start():
+def start(resources: list):
+    money = 0.0
     while True:
         response: str = input("What would you like? (espresso/latte/cappuccino): ")
 
         if response == "espresso":
-            pass
+            money, resources = process('espresso', resources, money)
         elif response == "latte":
-            pass
+            money, resources = process('latte', resources, money)
         elif response == "cappuccino":
-            pass
+            money, resources = process('cappuccino', resources, money)
         elif response == "report":
-            report()
+            report(money)
         elif response == "off":
             break
 
 
-def report():
+def process(name: str, sources: list, funds: float):
+
+    stock_flag, missing = in_stock(name, sources)
+
+    if stock_flag != True:
+        print(f"Sorry there is not enough {missing}")
+    else:
+        funds_flag, funds = process_coins(name, funds)
+        if funds_flag:
+            sources = process_drink(name, sources)
+            print("Here is your latte. Enjoy!")
+    
+    return funds, sources
+
+
+def in_stock(name: str, sources: list):
+    for k, v in MENU[name]['ingredients'].items():
+        if sources[k] < v:
+            return False, k
+            
+    return True, ""
+
+
+def process_coins(name: str, funds: str):
+    change = 0.0
+
+    print("Insert coins:")
+    quarters = float(input("Quarters: ")) * 0.25
+    dimes = float(input("Dimes: ")) * 0.10
+    nickles = float(input("Nickels: ")) * 0.05
+    pennies = float(input("Pennies: ")) * 0.01
+
+    total = quarters + dimes + nickles + pennies
+
+    if total < MENU[name]["cost"]:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+    elif total > MENU[name]["cost"]:
+        change = total - MENU[name]["cost"]
+        funds += MENU[name]["cost"]
+        print(f"Here is ${change} dollars in change.")
+    else:
+        funds += MENU[name]["cost"]
+    
+    return True, funds
+
+
+def process_drink(name: str, sources: list):
+    for k, v in MENU[name]['ingredients'].items():
+        sources[k] -= v
+
+    return sources
+
+
+def report(funds: float):
     print(f"Water: {resources['water']}ml")
     print(f"Milk: {resources['milk']}ml")
     print(f"Coffee: {resources['coffee']}g")
-    print(f"Money: ${money}")
+    print(f"Money: ${funds}")
 
-
-def drink(name: str, sources: list):
-    for k, v in MENU[name]['ingredients'].items():
-        print(f"{k.capitalize()}, {v}")
-        if sources[k] >= v:
-            print(k)
-
-
-#start()
+if __name__ == "__main__":
+    start(resources)
 # print(MENU['espresso']['ingredients']['water'])
-drink("espresso", resources)
+#in_stock("espresso", resources)
